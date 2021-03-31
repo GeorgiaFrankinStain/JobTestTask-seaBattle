4944085eac63cb194bda67d31c87fcff463735fb
Georgia FrankinStain <georgiafrankinstain@gmail.com>
2021-04-01 02:36:45 +0700

    add the ability to set the field size

--- sea_battle_react/src/App.js
+++ sea_battle_react/src/App.js
@@ -1,4 +1,5 @@
 import NameForm from './NameForm';
+import NewGameHeight from './NewGameHeight';
 import './App.css';
 
 
@@ -8,12 +9,17 @@ function App() {
 <div class="container" id='container-id'>
   <table class="table-battlefield">
     <tr>
+      <td rowspan="2">
+        <div class="title_new_game">Параметры новой игры:</div>
+        <NewGameHeight />
+      </td>
+      <td rowspan="2" id="td_margin"></td>
       <td><div class="title-battlefield myname" id="name_player"><NameForm /></div></td>
       <td><div class="title-battlefield enemy-name" id="name_computer"><NameForm /></div></td>
     </tr>
     <tr>
       <td>
-        <div class="battlefield">
+        <div class="battlefield" id="my_battlefield">
           <table class="battlefield2">
             <tr>
               <td><div class="killed_ally">X</div></td>
@@ -27,7 +33,7 @@ function App() {
         </div>
       </td>
       <td class="td_with_enemy_map">
-        <div class="battlefield">
+        <div class="battlefield"  id="enemy_battlefield">
           <table class="battlefield2">
             <tr>
               <td><div class="killed_enemy">✔</div></td>
@@ -41,18 +47,24 @@ function App() {
         </div>
       </td>
     </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td colspan="2">
+        <table class="data_about_game">
+          <tr class="number_steps">
+            <td class="title-number-steps">(number of steps taken)</td>
+            <td class="number-steps">15</td>
+            <td rowspan="2" class="td_whose_turn"><div class="whose_turn">GFS<br/><span class="lowel_signature">move</span></div></td>
+          </tr>
+          <tr class="min_possible_number_steps_for_win">
+            <td class="title-number-steps">(minimum number of steps to win)</td>
+            <td class="number-steps">5</td>
+          </tr>
+        </table>
+      </td>
+    </tr>
   </table>    
-  <table class="data_about_game">
-    <tr class="number_steps">
-      <td class="title-number-steps">(number of steps taken)</td>
-      <td class="number-steps">15</td>
-      <td rowspan="2" class="td_whose_turn"><div class="whose_turn">GFS<br/><span class="lowel_signature">move</span></div></td>
-    </tr>
-    <tr class="min_possible_number_steps_for_win">
-      <td class="title-number-steps">(minimum number of steps to win)</td>
-      <td class="number-steps">5</td>
-    </tr>
-  </table>
 </div>
   );
 }
---
+++ sea_battle_react/src/NewGameHeight.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class NewGameHeight extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '10'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+   renderTableTr(cells) {
+        return cells.map((line, column) => {
+           return (
+                 <td>({line}, {column})</td>
+           )
+        })
+     }
+
+
+
+   renderTable() {
+      return this.tableArray.map((cells, line) => {
+         return (
+            <tr key="t">
+               {this.renderTableTr(cells)}
+            </tr>
+         )
+      })
+   }
+
+   reTable() {
+      return (
+         <div>
+            <table id='students'>
+               <tbody>
+                  {this.renderTable()}
+               </tbody>
+            </table>
+         </div>
+      )
+   }
+
+  handleSubmit(event) {
+    //TODO: add battlefield
+    var width =  document.getElementById("width_battlefield_input").value;
+    var height =  document.getElementById("height_battlefield_input").value;
+    this.tableArray = [];
+    for (var line = 0; line < height; line++) {
+      var lineArray = [];
+      for (var column = 0; column < width; column++) {
+        lineArray[column] = [line, column];
+      }
+      this.tableArray[line] = lineArray;
+    }
+    // alert(this.tableArray);
+    // alert(this.reTable());
+
+    ReactDOM.render(
+      this.reTable(),
+      document.getElementById('my_battlefield')
+    );
+    ReactDOM.render(
+      this.reTable(),
+      document.getElementById('enemy_battlefield')
+    );
+    event.preventDefault();
+  }
+
+
+
+  render() {
+    return (
+      <form id="new_game_form_height" onSubmit={this.handleSubmit}>
+        <table>
+          <tr>
+            <td>
+              Высота поля:
+            </td>
+            <td>
+              <label>
+                <input class="size_battlefield_input" id="height_battlefield_input" type="number" value={this.state.value} onChange={this.handleChange} />
+              </label>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Ширина поля:
+            </td>
+            <td>
+              <label>
+                <input class="size_battlefield_input" id="width_battlefield_input" type="number" value={this.state.value} onChange={this.handleChange} />
+              </label>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2"><input type="submit" value="Submit" /></td>
+          </tr>
+        </table>
+      </form>
+    );
+  }
+}
+
+
+
+
+
+
+export default NewGameHeight;
+
